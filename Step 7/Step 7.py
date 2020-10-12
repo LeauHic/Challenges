3b42dc6a39dd90d37290f560dcbda9cdd2788bad
Loick Brouard <loickbrouard@yahoo.fr>
2020-10-12 07:17:46 +0200

    Step 7 completion, comments missings gitignore modifications

--- Step 7/Step 7.py
+++ Step 7/Step 7.py
@@ -1,5 +1,17 @@
-import requests
-import pypng
-import re
+# http://www.pythonchallenge.com/pc/def/oxygen.html
 
-pypng.reader("oxygen.png")
+from PIL import Image
+
+txt = " "
+with Image.open("oxygen.png") as img:
+    pix = img.load()
+    width, height = img.size
+    i = 0
+    while i < width-21:
+        txt += chr((pix[i, height/2][0]))
+        i += 7
+    print(txt)
+
+solutionCoded = [105, 110, 116, 101, 103, 114, 105, 116, 121]
+solutionDecoded = [chr(x) for x in solutionCoded]
+print(solutionDecoded)
